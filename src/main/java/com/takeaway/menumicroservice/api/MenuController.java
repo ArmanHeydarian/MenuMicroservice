2fb55b778b564662af70c062ceba33be0d5b519f
Arman Heydarian <36139854+ArmanHeydarian@users.noreply.github.com>
2022-03-14 15:36:00 -0800

    menu Controler is add

---
+++ src/main/java/com/takeaway/menumicroservice/api/MenuController.java
@@ -0,0 +1,32 @@
+package com.takeaway.menumicroservice.api;
+
+import com.takeaway.menumicroservice.ExceptionHandler.RecordNotFoundException;
+import com.takeaway.menumicroservice.domain.dto.MenuDto;
+import com.takeaway.menumicroservice.domain.model.Menu;
+import com.takeaway.menumicroservice.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(path="/api/menu")
+public class MenuController {
+
+    @Autowired
+    private MenuService menuService;
+
+    @PostMapping(path = "/sync")
+    public ResponseEntity<String> syncMenu(@RequestBody MenuDto menuDto) throws Exception , MethodArgumentNotValidException {
+        return menuService.syncMenu(menuDto);
+    }
+
+    @GetMapping(path = "/get" , produces = {"application/json"})
+    public ResponseEntity<Menu> getMenuByResturantId(@RequestParam(value = "resturantId"  ) Integer resturantId) throws Exception , RecordNotFoundException {
+        Menu menu = menuService.getMenuByResturantId(resturantId);
+        return  ResponseEntity.ok().body(menu);
+
+    }
+
+}
